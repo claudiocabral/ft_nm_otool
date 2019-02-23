71b4217b6c46baa2a7bb2203cdd8b9941f97b09e
Claudio Cabral <cabral1349@gmail.com>
2019-02-23 16:54:49 +0100

    found name of sections

--- srcs/binary_library/parse64.c
+++ srcs/binary_library/parse64.c
@@ -6,13 +6,13 @@
 /*   By: ccabral <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 16:12:11 by ccabral           #+#    #+#             */
-/*   Updated: 2019/02/23 14:37:05 by ccabral          ###   ########.fr       */
+/*   Updated: 2019/02/23 16:52:49 by ccabral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <binary_loader.h>
 
-void	print64(const t_nlist_64 *list, const char *string_table)
+void	print64(const t_nlist_64 *list, const char *string_table, uint32_t text_section)
 {
 	char const	*type_table;
 	char		type;
@@ -25,12 +25,12 @@ void	print64(const t_nlist_64 *list, const char *string_table)
 		printf("                 %c %s\n",
 				type_table[list->n_type & N_TYPE],
 				string_table + list->n_un.n_strx);
-	else if (type == N_UNDF)
+	else if (type != N_SECT)
 		printf("%016llx %c %s\n",
 				list->n_value,
 				type_table[list->n_type & N_TYPE],
 				string_table + list->n_un.n_strx);
-	else
+	else if(list->n_sect == text_section)
 		printf("%016llx %c %s\n",
 				list->n_value,
 				type_table[list->n_type & N_TYPE],
@@ -39,7 +39,7 @@ void	print64(const t_nlist_64 *list, const char *string_table)
 
 
 void	print_symtab_command(const t_symtab_command *symbol_table,
-		const char *ptr, size_t max)
+				const char *ptr, uint32_t text_section, size_t max)
 {
 	const t_nlist_64	*symbols;
 	t_nlist_64 const	**list;
@@ -65,51 +65,70 @@ void	print_symtab_command(const t_symtab_command *symbol_table,
 	i = 0;
 	while (i < symbol_table->nsyms)
 	{
-		print64(list[i], string_table);
+		print64(list[i], string_table, text_section);
 		++i;
 	}
-	(void)symbol_table;
 }
 
-const t_segment_command_64	*get_segment_command_64(t_load_command *load,
-		uint32_t number_of_commands, size_t max)
+uint32_t	print_segment(const t_segment_command_64 *segment)
 {
-	uint32_t	i;
+	const struct section_64	*section;
+	uint32_t			i;
+
+	i = 0;
+	section = (const void *)segment + sizeof(t_segment_command_64);
+	while (i < segment->nsects)
+	{
+		printf("[%u]: %s\n", i, section->sectname);
+		++i;
+		section = (void *)section + section->size;
+	}
+	return (0);
+}
+
+uint32_t	get_text_section(t_segment_command_64 *load,
+				uint32_t number_of_commands, size_t max)
+{
+	(void)max;
+
+	uint32_t					i;
+	const t_segment_command_64	*segment;
 
 	i = 0;
 	while (i < number_of_commands)
 	{
-		if (load->cmd == LC_SEGMENT)
+		if (load->cmd == LC_SEGMENT_64)
 		{
-			return ((const t_segment_command_64 *)load);
+			segment = (void *)load;
+			printf("%s %llu\n", segment->segname, segment->fileoff);
+			print_segment(segment);
+					
 		}
 		load = (void *) load + load->cmdsize;
 		if ((size_t)load > max - (sizeof(t_load_command)))
 			break ;
 		++i;
 	}
-	return ((const t_segment_command_64 *)load);
+	return (0);
 }
 
 void	parse64(const t_mach_header_64 *file, size_t max)
 {
-	uint32_t					i;
-	t_load_command				*load;
-	const t_segment_command_64	*segment_table;
+	uint32_t			i;
+	t_load_command		*load;
+	uint32_t			text_section;
 
 	if (!file)
 		return ;
 	load = (t_load_command*)(file + 1);
-	segment_table = get_segment_command_64(load, file->ncmds, max);
-	if (!segment_table)
-		return ;
+	text_section = get_text_section((void *)load, file->ncmds, max);
 	i = 0;
 	while (i < file->ncmds)
 	{
 		if (load->cmd == LC_SYMTAB)
 		{
 			print_symtab_command((const t_symtab_command *)load,
-					(const char *)file, max);
+					(const char *)file, text_section, max);
 			break ;
 		}
 		load = (void *) load + load->cmdsize;
