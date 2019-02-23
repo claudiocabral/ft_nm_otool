653882dbc0a0c0cc237db5449962b98fe57e45f1
Claudio Cabral <cabral1349@gmail.com>
2019-02-23 20:21:02 +0100

    added get_number_of_sections function

--- srcs/binary_library/segments.c
+++ srcs/binary_library/segments.c
@@ -6,7 +6,7 @@
 /*   By: ccabral <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 18:00:57 by ccabral           #+#    #+#             */
-/*   Updated: 2019/02/23 20:13:19 by ccabral          ###   ########.fr       */
+/*   Updated: 2019/02/23 20:20:42 by ccabral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,27 +27,30 @@ uint32_t	print_segment(const t_segment_command_64 *segment)
 	}
 	return (0);
 }
+
 uint32_t	get_number_of_sections(t_load_command *load,
 				uint32_t number_of_commands, t_abstract_mach *header)
 {
 	uint32_t					i;
 	uint32_t					total;
+	uint32_t					cmd;
 
 	i = 0;
 	total = 0;
 	while (i < number_of_commands)
 	{
-		if (load->cmd == LC_SEGMENT_64)
-		{
-			print_segment((t_segment_command_64 *)load);
-		}
-		load = (void *) load + load->cmdsize;
+		cmd = endianless(header->big_endian, load->cmd);
+		if (cmd == LC_SEGMENT_64)
+			total += endianless(header->big_endian, ((t_segment_command_64 *)load)->nsects);
+		if (cmd == LC_SEGMENT)
+			total += endianless(header->big_endian, ((t_segment_command *)load)->nsects);
+		load = (void *) load + endianless(header->big_endian, load->cmdsize);
 		if (!is_in_file(load, sizeof(t_load_command),
 					header->file, header->file_size))
 			break ;
 		++i;
 	}
-	return (0);
+	return (total);
 }
 
 uint32_t	get_text_section(t_load_command *load,
