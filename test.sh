1aa6666267afefa1f08ff0d293729d722b6a817a
Claudio Cabral <cabral1349@gmail.com>
2019-02-26 13:35:34 +0100

    added test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function test_file() {
+	echo testing file $1
+	diff <(./ft_nm $1) <(nm $1)
+	if [ $? = 0 ]
+	then
+		echo OK
+	fi
+}
+
+test_file objs/nm/main.o
+test_file ft_nm
+test_file /usr/bin/audiodevice
+test_file ../ft_malloc_tests/ft_malloc/libft_malloc.so
