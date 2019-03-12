0994d675f07b8bd60dce6651cfb1238c419cdfb5
Claudio Cabral <cabral1349@gmail.com>
2019-03-12 11:40:33 +0100

    safer print

--- srcs/binary_library/print.c
+++ srcs/binary_library/print.c
@@ -6,11 +6,30 @@
 /*   By: ccabral <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 18:20:30 by ccabral           #+#    #+#             */
-/*   Updated: 2019/03/04 16:06:43 by ccabral          ###   ########.fr       */
+/*   Updated: 2019/03/12 11:24:16 by ccabral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <binary_loader.h>
+
+int		check_table(const char *string_table, uint64_t offset, const char *eof)
+{
+	uint64_t	i;
+
+	if ((string_table + offset >= eof))
+		return (0);
+	if (!string_table[offset])
+		return (0);
+	i = 0;
+	string_table += offset;
+	while (string_table + i < eof)
+	{
+		if (!string_table[i])
+			return (1);
+		++i;
+	}
+	return (0);
+}
 
 void	print32(const t_nlist *list, t_abstract_mach *header,
 		const char *string_table)
@@ -25,9 +44,7 @@ void	print32(const t_nlist *list, t_abstract_mach *header,
 		return ;
 	if (type == 'U' && list->n_value)
 		type = 'C';
-	if ((uint64_t)string_table + offset >= header->eof)
-		return ;
-	if (!string_table[offset])
+	if (!check_table(string_table, offset, (const char *)header->eof))
 		return ;
 	if (type == 'U')
 		ft_printf("         %c %s\n",
@@ -44,15 +61,15 @@ void	print64(const t_nlist_64 *list, t_abstract_mach *header,
 		const char *string_table)
 {
 	char		type;
+	uint64_t	offset;
 
+	offset = endianless(header->big_endian, list->n_un.n_strx);
 	type = get_type_symbol(list, header);
 	if (type == '-')
 		return ;
 	else if (type == 'U' && list->n_value)
 		type = 'C';
-	if ((uint64_t)string_table + list->n_un.n_strx >= header->eof)
-		return ;
-	if (!string_table[list->n_un.n_strx])
+	if (!check_table(string_table, offset, (const char *)header->eof))
 		return ;
 	if (type == 'U')
 		ft_printf("                 %c %s\n",
